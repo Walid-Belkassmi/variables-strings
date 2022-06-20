18a0f4dfab744572c662e530f9f4bb2cea70556e
Walid-Belkassmi <belkassmi.walid@gmail.com>
2022-06-20 12:36:35 +0200

    done

---
+++ index.js
@@ -0,0 +1,39 @@
+// 01 - Hello World
+// console.log("Hello World !")
+
+// 02 - String
+// const sentence = "My name is Walid"
+// console.log(sentence)
+
+// 03 - Concaténation
+// const name = "Walid"
+// console.log("Nice to meet you" + "name")
+
+// 04 - String Length
+// const sentence = "I'm a new text ! "
+// console.log(sentence.length)
+
+// 05 - Replace
+// let food = "croissant is meh"
+// food = food.replace("meh", "so good")
+// console.log(food)
+
+// 06 - Up and Down 
+// const basic = "This is Cool"
+// console.log(basic)
+
+// const basicUp = basic.toUpperCase()
+// console.log(basicUp)
+
+// const basicDown = basic.toLocaleLowerCase()
+// console.log(basicDown)
+
+// 07 - Split
+// const word = "banana"
+// const letters = word.split('')
+// console.log(letters)
+
+// 08 - Templates
+// const age = 25
+// const template = `I'm ${age} years old`
+// console.log(template)
